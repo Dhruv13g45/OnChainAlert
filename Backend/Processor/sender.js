6d32a14dd483e6ceda5739603a55eb0e3972eca6
druv puraswani <puraswanidruv@gmail.com>
2026-02-06 14:56:59 +0530

    complete processor module

--- Backend/Processor/sender.js
+++ Backend/Processor/sender.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function sendToDatabase(event) {
   try {
     console.log("📦 Sending event:", event);
-    // await axios.post("http://localhost:5000/events", event);
+    await axios.post("http://localhost:8000/events", event);
   } catch (err) {
     console.log("⚠️ Backend not running (safe during demo)");
   }
